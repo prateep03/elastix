5047b38922d7dc8233881b51a8b87be184f5f442
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-12-03 15:45:23 -0500

    Made MEVIS tiff conditional

--- src/Core/Main/elastix.cxx
+++ src/Core/Main/elastix.cxx
@@ -64,9 +64,11 @@ int main( int argc, char **argv )
   typedef std::queue< ArgPairType >                   ParameterFileListType;
   typedef ParameterFileListType::value_type           ParameterFileListEntryType;
 
+#ifdef ELASTIX_USE_MEVIS  
   /** Support Mevis Dicom Tiff (if selected in cmake) */
   RegisterMevisDicomTiff();
-
+#endif //ELASTIX_USE_MEVIS  
+  
   /** Some declarations and initialisations. */
   ElastixMainVectorType elastices;
 
